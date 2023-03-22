e5a09bc084324de424bc6bd13c333a7e65fdcb5d
Adhurhagbemie <daskillzedu@gmail.com>
2023-03-22 04:07:57 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabeth - function to print abc
+ *
+ * Return: 0
+ */
+void print_alphabeth(void)
+{
+	char c;
+
+	for (c = 'a' c <= 'z'; c++)
+}
+        {
+		_putchar(c);
+	}
+_putchar('\n');
+}
